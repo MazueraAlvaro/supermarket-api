c10fa97923d800f0857e0eea0b62ad72eb5cfd00
Alvaro Mazuera U <alvaromazuera16@gmail.com>
2020-12-20 14:44:45 -0500

    Se añade CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const { config } = require('./config/index');
@@ -11,6 +12,7 @@ const {
 } = require('./utils/middleware/errorHandlers');
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
+app.use(cors());
 app.use(express.json());
 
 productsApi(app);
